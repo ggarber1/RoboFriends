771d64ae9c3d2e7d01394332512f42a6f6d6bc3b
Gregory Garber <gregorygarber@Gregorys-MacBook-Air.local>
2021-01-22 12:03:38 -0500

    first commit

---
+++ src/components/CardList.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Card from './Card';
+
+const CardList = ({ Robots }) => {
+	const cardArray = Robots.map((user, i) => {
+		return (
+			<Card 
+			key={i} 
+			id={Robots[i].id} 
+			name={Robots[i].name} 
+			email={Robots[i].email}
+			/>
+		)
+	})
+	return (
+		<div>
+		{cardArray}
+		</div>
+	);
+}
+
+export default CardList;
